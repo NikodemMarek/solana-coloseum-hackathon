23435378d656ea3a6f2c432a4d4661b01c1c15f3
Plasny <git.plasny.uq95y@slmail.me>
2024-04-07 17:15:45 +0200

    feat: idea card and idea grid restyling

--- app/src/components/IdeasGrid.tsx
+++ app/src/components/IdeasGrid.tsx
@@ -2,15 +2,20 @@ import Masonry from "@mui/lab/Masonry";
 import { Idea } from "../data/data.ts";
 import IdeaCard from "./IdeaCard";
 
-const IdeasGrid = ({ ideas }: { ideas: Idea[] }) => {
-  return (
-
-    <Masonry columns={window.innerWidth >= 640 ? 4 : 1} spacing={2}>
-      {ideas.map((idea, key) => (
-        <IdeaCard key={key} idea={idea} />
-      ))}
-    </Masonry>
-  );
+const IdeasGrid = ({ ideas, forSale }: { ideas: Idea[], forSale: boolean }) => {
+    return window.innerWidth >= 640 ? (
+        <Masonry columns={4} spacing={2}>
+            {ideas.map((idea, key) => (
+                <IdeaCard key={key} idea={idea} forSale={forSale} />
+            ))}
+        </Masonry>
+    ) : (
+        <div className="w-full flex flex-col items-center space-y-4">
+            {ideas.map((idea, key) => (
+                <IdeaCard key={key} idea={idea} forSale={forSale} />
+            ))}
+        </div>
+    )
 };
 
 export default IdeasGrid;
